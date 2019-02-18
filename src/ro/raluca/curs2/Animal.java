d5376e0376499d94f78dcd01f1ce9278797abfb7
Raluca Mafteciuc <Raluca.Mafteciuc@hartehanks.com>
2019-02-18 17:45:56 +0200

    Curs 2

---
+++ src/ro/raluca/curs2/Animal.java
@@ -0,0 +1,31 @@
+package ro.raluca.curs2;
+
+abstract public class Animal {
+	
+	//properties
+	private int age;
+	private String breed;
+	
+	//methods
+	//abstract methods
+	abstract public void eat();
+	
+	public void says() {
+		System.out.println("This animal says a lot of stuff!");
+	}
+	
+	public int getAge() {
+		return this.age;
+	}	
+	public String getBreed() {
+		return this.breed;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}	
+	public void setBreed(String breed) {
+		if ("".equals(breed)) this.breed = "kitty";
+		else this.breed = breed;
+	}		
+}
---
+++ src/ro/raluca/curs2/TestCat.java
@@ -0,0 +1,26 @@
+package ro.raluca.curs2;
+
+import java.util.Scanner;
+
+class TestCat {
+	public static void main(String args[]) {	
+		Cat co = new Cat();		
+		Scanner input = new Scanner(System.in);
+	    
+		System.out.println("Enter your cat's name:");
+		co.setName(input.nextLine());
+		
+		System.out.println("Enter your cat's breed:");
+		co.setBreed(input.nextLine());
+		
+		System.out.println("How old is your cat?");
+		co.setAge(input.nextInt());			
+		
+		System.out.println(co.toString());
+		
+		co.eat();
+		co.says();
+		
+		input.close();
+	}
+}
